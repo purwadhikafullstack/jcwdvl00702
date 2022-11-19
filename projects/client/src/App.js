657f309af465c8075e50ad7d41c7e9d78cf50d7d
Cychlope <m.marcelinus@gmail.com>
2022-11-19 22:20:07 +0700

    Feature MWA-41: Rev-1

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+<<<<<<< HEAD
         <Switch>
           <Route component={SignIn} path="/sign-in" />
           <Route component={SignUp} path="/sign-up" />
@@ -32,6 +33,17 @@ class App extends React.Component {
           <Route component={HomePage} path="/" />
         </Switch>
         <Footer />
+=======
+        <Route component={SignIn} path="/sign-in" />
+        <Route component={SignUp} path="/sign-up" />
+        <Route component={CreatePassword} path="/create-password" />
+        <Route component={MyOrder} path="/my-order" />
+        <Route component={ProductDetail} path="/product-detail" />
+        <Route component={ChangePassword} path="/change-password" />
+        <Route component={ResetPassword} path="/reset-password" />
+        <Route component={Profile} path="/profile" />
+        <Route component={EditProfile} path="/edit-profile" />
+>>>>>>> 6d11b552 (Feature MWA-41: Rev-1)
       </BrowserRouter>
     );
   }
