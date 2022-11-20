a62b94246ec9196aea8f5ae94cd7aedb9b2e3de7
Cychlope <m.marcelinus@gmail.com>
2022-11-21 00:59:55 +0700

    Feature MWA-41: Rev.2

--- projects/client/src/Pages/HomePage.js
+++ projects/client/src/Pages/HomePage.js
@@ -1,79 +1,85 @@
-import "../assets/styles/HomePage.css"
-import * as React from "react"
-import Avatar from "@mui/material/Avatar"
-import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import IconButton from "@mui/material/IconButton";
-import InputBase from "@mui/material/InputBase";
-import SearchIcon from "@mui/icons-material/Search";
-import SportsSoccerOutlinedIcon from "@mui/icons-material/SportsSoccerOutlined";
-import BusinessCenterOutlinedIcon from "@mui/icons-material/BusinessCenterOutlined";
-import DirectionsBikeOutlinedIcon from "@mui/icons-material/DirectionsBikeOutlined";
-import HikingOutlinedIcon from "@mui/icons-material/HikingOutlined";
-import HandymanOutlinedIcon from "@mui/icons-material/HandymanOutlined";
-import {Navbar, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, NavbarBrand, NavbarText, DropdownMenu, DropdownItem} from 'reactstrap';
-import LogoutIcon from "@mui/icons-material/Logout";
-import { Slide } from "react-slideshow-image";
+import '../assets/styles/HomePage.css';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import SportsSoccerOutlinedIcon from '@mui/icons-material/SportsSoccerOutlined';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
+import DirectionsBikeOutlinedIcon from '@mui/icons-material/DirectionsBikeOutlined';
+import HikingOutlinedIcon from '@mui/icons-material/HikingOutlined';
+import HandymanOutlinedIcon from '@mui/icons-material/HandymanOutlined';
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  NavbarBrand,
+  NavbarText,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
-import "../assets/styles/ProductLists.css";
-import { Tooltip, Menu, MenuItem, Button } from "@mui/material";
-import PopupState, {bindTrigger, bindMenu} from "material-ui-popup-state";
-
+import '../assets/styles/ProductLists.css';
+import { Tooltip, Menu, MenuItem, Button } from '@mui/material';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 
 class HomePage extends React.Component {
-
   render() {
     const slideCarousels = [
       {
-        url: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
+        url: 'https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg',
       },
       {
-        url: "https://media.timeout.com/images/105240237/image.jpg",
+        url: 'https://media.timeout.com/images/105240237/image.jpg',
       },
     ];
 
     const ListProducts = [
       {
-        image:
-          "https://dks.scene7.com/is/image/GolfGalaxy/22ADIUWC2022CMP5XSCB_White_Pantone?qlt=70&wid=600&fmt=pjpeg",
-        description: "Ball",
-        price: "900.000",
+        image: 'https://dks.scene7.com/is/image/GolfGalaxy/22ADIUWC2022CMP5XSCB_White_Pantone?qlt=70&wid=600&fmt=pjpeg',
+        description: 'Ball',
+        price: '900.000',
       },
       {
-        image: "https://cf.shopee.co.id/file/d7622a165c1b915b19e63e1ebd246ba4",
-        description: "Adidas Bag",
-        price: "300.000",
+        image: 'https://cf.shopee.co.id/file/d7622a165c1b915b19e63e1ebd246ba4',
+        description: 'Adidas Bag',
+        price: '300.000',
       },
       {
-        image:
-          "https://www.wilson.com/en-us/media/catalog/product/W/T/WTB7500ID__b722ae318490e0f2e686864dc70fd730.png",
-        description: "Wilson Basketball",
-        price: "400.000",
-      },
-      {
-        image:
-          "https://id.brompton.com/-/media/sections/my22/ux-project/cutout-bikes/c-line/c-line-659x437.ashx?h=437&w=659&la=id-ID&hash=D96DD87397FB04775257AD8C1814A20B",
-        description: "Brompton Bicycle",
-        price: "65.000.000",
+        image: 'https://www.wilson.com/en-us/media/catalog/product/W/T/WTB7500ID__b722ae318490e0f2e686864dc70fd730.png',
+        description: 'Wilson Basketball',
+        price: '400.000',
       },
       {
         image:
-          "https://cdn.eraspace.com/pub/media/catalog/product/a/p/apple_watch_series_7_41mm_midnight_sport_band_1_1.jpg",
-        description: "Apple Watch Series 7",
-        price: "7.000.000",
+          'https://id.brompton.com/-/media/sections/my22/ux-project/cutout-bikes/c-line/c-line-659x437.ashx?h=437&w=659&la=id-ID&hash=D96DD87397FB04775257AD8C1814A20B',
+        description: 'Brompton Bicycle',
+        price: '65.000.000',
       },
       {
         image:
-          "https://m.media-amazon.com/images/I/31sjSruHyIL._AC_SY1000_.jpg",
-        description: "Ball Pump",
-        price: "150.000",
+          'https://cdn.eraspace.com/pub/media/catalog/product/a/p/apple_watch_series_7_41mm_midnight_sport_band_1_1.jpg',
+        description: 'Apple Watch Series 7',
+        price: '7.000.000',
+      },
+      {
+        image: 'https://m.media-amazon.com/images/I/31sjSruHyIL._AC_SY1000_.jpg',
+        description: 'Ball Pump',
+        price: '150.000',
       },
       {
         image:
-          "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a48c5834-daa2-4ef6-b04d-19a04fcda866/heritage-backpack-cPq0Vl.png",
-        description: "Nike Bag",
-        price: "299.000",
+          'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a48c5834-daa2-4ef6-b04d-19a04fcda866/heritage-backpack-cPq0Vl.png',
+        description: 'Nike Bag',
+        price: '299.000',
       },
     ];
     return (
@@ -82,7 +88,7 @@ class HomePage extends React.Component {
           <div className="homepage">
             <div className="homepage-top">
               <PopupState variant="popover" popupId="demo-popup-menu">
-                {(popupState)=> (
+                {(popupState) => (
                   <React.Fragment>
                     <button className="account-button" variant="contained" {...bindTrigger(popupState)}>
                       <Avatar>
@@ -90,8 +96,14 @@ class HomePage extends React.Component {
                       </Avatar>
                     </button>
                     <Menu {...bindMenu(popupState)}>
-                      <MenuItem onClick={popupState.close}><AccountBoxIcon />Profile</MenuItem>
-                      <MenuItem onClick={popupState.close}><LogoutIcon />Sign Out</MenuItem>
+                      <MenuItem onClick={popupState.close}>
+                        <AccountBoxIcon />
+                        Profile
+                      </MenuItem>
+                      <MenuItem onClick={popupState.close}>
+                        <LogoutIcon />
+                        Sign Out
+                      </MenuItem>
                     </Menu>
                   </React.Fragment>
                 )}
@@ -110,10 +122,10 @@ class HomePage extends React.Component {
               <InputBase
                 sx={{ ml: 1, flex: 1 }}
                 placeholder="Search"
-                inputProps={{ "aria-label": "Search" }}
+                inputProps={{ 'aria-label': 'Search' }}
                 className="search-bar"
               />
-              <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+              <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
                 <SearchIcon />
               </IconButton>
             </div>
@@ -176,4 +188,4 @@ class HomePage extends React.Component {
   }
 }
 
-export default HomePage
+export default HomePage;
