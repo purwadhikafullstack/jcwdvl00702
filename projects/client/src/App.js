b3417228d1d5d146d276a2f3188552a217f36eee
Cychlope <m.marcelinus@gmail.com>
2022-11-21 17:43:02 +0700

    rebase MWA45

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -10,13 +10,6 @@ import ProductDetail from './pages/ProductDetail';
 import ChangePassword from './pages/Auth/PasswordChange/ChangePassword';
 import ResetPassword from './pages/Auth/PasswordChange/ResetPassword';
 <<<<<<< HEAD
-import Profile from './pages/Profile';
-import EditProfile from './pages/EditProfile';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import ProductLists from './pages/ProductLists';
-=======
-<<<<<<< HEAD
 import Profile from './pages/Profile'
 import EditProfile from './pages/EditProfile';
 import Footer from "./components/Footer"
@@ -25,14 +18,14 @@ import ProductLists from "./pages/ProductLists"
 =======
 import Profile from './pages/Profile';
 import EditProfile from './pages/EditProfile';
->>>>>>> 0a0d76ad (Feature MWA-41: Rev-1)
->>>>>>> 94694b31 (Feature MWA-41: Rev-1)
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductLists from './pages/ProductLists';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-<<<<<<< HEAD
         <Switch>
           <Route component={SignIn} path="/sign-in" />
           <Route component={ForgotPassword} path="/forgot-password" />
@@ -48,18 +41,6 @@ class App extends React.Component {
           <Route component={HomePage} path="/" />
         </Switch>
         <Footer />
-=======
-        <Route component={SignIn} path="/sign-in" />
-        <Route component={ForgotPassword} path="/forgot-password" />
-        <Route component={SignUp} path="/sign-up" />
-        <Route component={CreatePassword} path="/create-password" />
-        <Route component={MyOrder} path="/my-order" />
-        <Route component={ProductDetail} path="/product-detail" />
-        <Route component={ChangePassword} path="/change-password" />
-        <Route component={ResetPassword} path="/reset-password" />
-        <Route component={Profile} path="/profile" />
-        <Route component={EditProfile} path="/edit-profile" />
->>>>>>> 6d11b552 (Feature MWA-41: Rev-1)
       </BrowserRouter>
     );
   }
