d602bfb9ced015ddecb2fdd986a38168debffcb7
Cychlope <m.marcelinus@gmail.com>
2022-11-21 17:30:51 +0700

    Features MWA-45 Rev.1

--- projects/client/src/pages/MyOrder.js
+++ projects/client/src/pages/MyOrder.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import { IconButton, Box, Tab, Tabs, Button, Container } from '@mui/material';
-import { PendingOutlined, ReceiptLong, Search } from '@mui/icons-material';
+import {
+  IconButton,
+  Box,
+  Tab,
+  Tabs,
+  Button,
+  Container,
+  Tooltip,
+  Menu,
+  MenuItem,
+  Avatar,
+  Popover,
+  Typography,
+  InputBase,
+  InputAdornment,
+  ClickAwayListener,
+  Pagination,
+  Stack,
+} from '@mui/material';
+import {
+  PendingOutlined,
+  ReceiptLong,
+  Search,
+  SortTwoTone,
+  AccountBox,
+  Logout,
+  LocalShipping,
+  Redeem,
+} from '@mui/icons-material';
 import { TabPanel, TabList, TabContext } from '@mui/lab';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 
 import '../assets/styles/MyOrder.css';
 
@@ -10,14 +38,65 @@ class MyOrder extends React.Component {
     isOnGoing: true,
     isCompleted: false,
     value: '1',
-    orderCompleted: true,
+    isOrderComplete: true,
+    isOrderOg: false,
+    status: '',
+    isSearch: false,
   };
 
   handleChange = (event, value) => {
     this.setState({ ...this.state, value });
   };
 
-  myOrderCard = () => {
+  isSearchHandle = () => {
+    this.setState({ ...this.state, isSearch: true });
+  };
+
+  isSearchHandleClose = () => {
+    this.setState({ ...this.state, isSearch: false });
+  };
+
+  mocDetailStatus = (status) => {
+    if (status === '1') {
+      return (
+        <Box className="moc-detail-status-1" sx={{ backgroundColor: 'rgb(255,165,0,0.4)' }}>
+          Menunggu pembayaran
+        </Box>
+      );
+    } else if (status === '2') {
+      return (
+        <Box className="moc-detail-status-2" sx={{ backgroundColor: 'rgb(255,215,0,0.4)' }}>
+          Menunggu konfirmasi
+        </Box>
+      );
+    } else if (status === '3') {
+      return (
+        <Box className="moc-detail-status-3" sx={{ backgroundColor: 'rgb(152,251,152,0.4)' }}>
+          Dalam proses
+        </Box>
+      );
+    } else if (status === '4') {
+      return (
+        <Box className="moc-detail-status-4" sx={{ backgroundColor: 'rgba(127, 255, 212, 0.4)' }}>
+          Dalam pengiriman
+        </Box>
+      );
+    } else if (status === '5') {
+      return (
+        <Box className="moc-detail-status-5" sx={{ backgroundColor: 'rgb(72,209,204,0.4)' }}>
+          Pesanan diterima
+        </Box>
+      );
+    } else {
+      return (
+        <Box className="moc-detail-status-6" sx={{ backgroundColor: 'rgb(220,20,60,0.4)' }}>
+          Pesanan dibatalkan
+        </Box>
+      );
+    }
+  };
+
+  myOrderCard = (status) => {
     return (
       <div className="moc-main">
         <div className="moc-image">
@@ -29,17 +108,15 @@ class MyOrder extends React.Component {
         </div>
         <div className="moc-detail">
           <div className="moc-detail-name">Kocheng Kochengan Lucu Aja</div>
-          <div className="moc-detail-subname">Si Belang | Cat | Male</div>
-          <Button className="moc-detail-status" sx={{ fontSize: '8px' }} variant="contained" disabled>
-            In delivery
-          </Button>
+          <div className="moc-detail-subname">Si Belang | Cat | Male | 29 ekor</div>
+          {this.mocDetailStatus(status)}
           <div className="moc-detail-bottom">
             <div className="moc-detail-bottom-price">Rp. 7.250.000 ,-</div>
             <Button
-              sx={{ borderRadius: '20px', backgroundColor: 'black', fontSize: '8px' }}
+              sx={{ borderRadius: '20px', backgroundColor: 'black', fontSize: '8px', fontFamily: 'Lora' }}
               variant="contained"
               className="moc-detail-bottom-track">
-              Track Order
+              Track order
             </Button>
           </div>
         </div>
@@ -53,20 +130,62 @@ class MyOrder extends React.Component {
         <div className="my-order-main">
           <div className="my-order-banner">
             <div className="my-order-banner-logo">
-              <IconButton>
+              <IconButton disabled>
                 <ReceiptLong />
               </IconButton>
             </div>
-            <div className="my-order-banner-text">My Orders</div>
-            <div className="my-order-banner-search">
-              <IconButton>
-                <Search />
-              </IconButton>
-            </div>
+
+            {this.state.isSearch ? (
+              <>
+                <ClickAwayListener onClickAway={this.isSearchHandleClose}>
+                  <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Search"
+                    inputProps={{ 'aria-label': 'Search' }}
+                    className="my-order-search"
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton edge="end">
+                          <Search />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </ClickAwayListener>
+              </>
+            ) : (
+              <>
+                <div className="my-order-banner-text">My Orders</div>
+                <div className="my-order-banner-search">
+                  <IconButton onClick={this.isSearchHandle}>
+                    <Search />
+                  </IconButton>
+                </div>
+              </>
+            )}
+
             <div className="my-order-banner-menu">
-              <IconButton>
-                <PendingOutlined />
-              </IconButton>
+              <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                  <React.Fragment>
+                    <button className="account-button" variant="contained" {...bindTrigger(popupState)}>
+                      <IconButton>
+                        <SortTwoTone />
+                      </IconButton>
+                    </button>
+                    <Menu {...bindMenu(popupState)}>
+                      <MenuItem onClick={popupState.close}>
+                        <img src="https://img.icons8.com/fluency-systems-filled/22/null/sort-numeric-up.png" />
+                        Oldest
+                      </MenuItem>
+                      <MenuItem onClick={popupState.close}>
+                        <img src="https://img.icons8.com/windows/24/null/sort-numeric-up-reversed.png" />
+                        Recent
+                      </MenuItem>
+                    </Menu>
+                  </React.Fragment>
+                )}
+              </PopupState>
             </div>
           </div>
 
@@ -75,29 +194,55 @@ class MyOrder extends React.Component {
               <TabContext value={this.state.value}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                   <TabList onChange={this.handleChange} aria-label="lab API tabs example">
-                    <Tab sx={{ marginLeft: '20px' }} label="On Going" value="1" />
-                    <Tab sx={{ marginLeft: '70px' }} label="Completed" value="2" />
+                    <Tab sx={{ marginLeft: '20px', fontFamily: 'Lora' }} label="On Going" value="1" />
+                    <Tab sx={{ marginLeft: '100px', fontFamily: 'Lora' }} label="Completed" value="2" />
                   </TabList>
                 </Box>
-                <TabPanel value="1">
-                  <div className="my-order-og">
-                    <img
-                      src="https://i.pinimg.com/originals/6f/df/bc/6fdfbc41d6a8e26d4b9073bc1afd899f.jpg"
-                      className="my-order-og-logo"
-                      alt="No Order"
-                    />
-                    <div className="my-order-og-text-1">You don't have an order yet</div>
-                    <div className="my-order-og-text-2">You don't have On Going orders at this time</div>
-                  </div>
-                </TabPanel>
 
-                {this.state.orderCompleted ? (
-                  <TabPanel value="2">
-                    {this.myOrderCard()}
-                    {this.myOrderCard()}
-                    {this.myOrderCard()}
-                    {this.myOrderCard()}
+                {this.state.isOrderOg ? (
+                  <>
+                    <TabPanel value="1">
+                      {this.myOrderCard('1')}
+                      {this.myOrderCard('2')}
+                      {this.myOrderCard('3')}
+                      {/* {this.myOrderCard('4')}
+                      {this.myOrderCard('5')}
+                      {this.myOrderCard('6')} */}
+
+                      <Stack spacing={1} sx={{ width: '110%' }}>
+                        <Pagination count={10} />
+                      </Stack>
+                    </TabPanel>
+                  </>
+                ) : (
+                  <TabPanel value="1">
+                    <div className="my-order-og">
+                      <img
+                        src="https://i.pinimg.com/originals/6f/df/bc/6fdfbc41d6a8e26d4b9073bc1afd899f.jpg"
+                        className="my-order-og-logo"
+                        alt="No Order"
+                      />
+                      <div className="my-order-og-text-1">You don't have an order yet</div>
+                      <div className="my-order-og-text-2">You don't have On Going orders at this time</div>
+                    </div>
                   </TabPanel>
+                )}
+
+                {this.state.isOrderComplete ? (
+                  <>
+                    <TabPanel value="2">
+                      {/* {this.myOrderCard('1')}
+                      {this.myOrderCard('2')}
+                      {this.myOrderCard('3')} */}
+                      {this.myOrderCard('4')}
+                      {this.myOrderCard('5')}
+                      {this.myOrderCard('6')}
+
+                      <Stack spacing={1} sx={{ width: '110%' }}>
+                        <Pagination count={10} />
+                      </Stack>
+                    </TabPanel>
+                  </>
                 ) : (
                   <TabPanel value="2">
                     <div className="my-order-og">
