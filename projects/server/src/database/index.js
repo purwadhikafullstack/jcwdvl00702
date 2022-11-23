8ab027126a03c588e69af6bdbc9498e52067c920
unknown <glenchosua@gmail.com>
2022-11-23 18:54:59 +0700

    sql connection setup

---
+++ projects/server/src/database/index.js
@@ -0,0 +1,19 @@
+// const mysql = require(`mysql2`);
+
+// const db = mysql.createConnection({
+//   host: `localhost`,
+//   user: `root`,
+//   password: `jcwdvl07`,
+//   database: `db_ecom`,
+//   port: 3306,
+//   multipleStatements: true,
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     return console.error(`error : ${err.message}`);
+//   }
+//   console.log(`Connected to MySQL Server`);
+// });
+
+// module.exports = { db };
--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -1,74 +1,42 @@
-require("dotenv/config");
-const express = require("express");
-const cors = require("cors");
-const { join } = require("path");
+const express = require(`express`);
+const cors = require(`cors`);
+const mysql = require("mysql2");
 
-const PORT = process.env.PORT || 8000;
+const PORT = 3300;
 const app = express();
-app.use(
-  cors({
-    origin: [
-      process.env.WHITELISTED_DOMAIN &&
-        process.env.WHITELISTED_DOMAIN.split(","),
-    ],
-  })
-);
 
+app.use(cors());
 app.use(express.json());
 
-//#region API ROUTES
+// const { karyawanRouters } = require(`./routers`);
+// app.use(`/karyawan`, karyawanRouters);
 
-// ===========================
-// NOTE : Add your routes here
-
-app.get("/api", (req, res) => {
-  res.send(`Hello, this is my API`);
+const db = mysql.createConnection({
+  host: `localhost`,
+  user: `root`,
+  password: `jcwdvl07`,
+  database: `db_ecom`,
+  port: 3306,
+  multipleStatements: true,
 });
 
-app.get("/api/greetings", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello, Student !",
+db.connect((err) => {
+  if (err) {
+    return console.error(`error : ${err.message}`);
+  }
+  console.log(`Connected to MySQL Server`);
+});
+
+app.get(`/`, (req, res) => {
+  res.status(200).send(`<h4>Integrated mysql with express</h4>`);
+});
+
+app.get("/customer", (req, res) => {
+  let scriptQuery = "Select * from customer";
+  db.query(scriptQuery, (err, results) => {
+    if (err) res.status(500).send(err);
+    res.status(200).send(results);
   });
 });
 
-// ===========================
-
-// not found
-app.use((req, res, next) => {
-  if (req.path.includes("/api/")) {
-    res.status(404).send("Not found !");
-  } else {
-    next();
-  }
-});
-
-// error
-app.use((err, req, res, next) => {
-  if (req.path.includes("/api/")) {
-    console.error("Error : ", err.stack);
-    res.status(500).send("Error !");
-  } else {
-    next();
-  }
-});
-
-//#endregion
-
-//#region CLIENT
-const clientPath = "../../client/build";
-app.use(express.static(join(__dirname, clientPath)));
-
-// Serve the HTML page
-app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, clientPath, "index.html"));
-});
-
-//#endregion
-
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log(`ERROR: ${err}`);
-  } else {
-    console.log(`APP RUNNING at ${PORT} ✅`);
-  }
-});
+app.listen(PORT, () => console.log(`API running:`, PORT));
