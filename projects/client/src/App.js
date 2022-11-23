1a4d735d03bd19a604fde21c9f169a41b3c94510
Cychlope <m.marcelinus@gmail.com>
2022-11-23 19:03:27 +0700

    rebase

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-import SignIn from "./pages/Auth/SignIn";
-import SignUp from "./pages/Auth/SignUp";
-import CreatePassword from "./pages/Auth/CreatePassword";
-import MyOrder from "./pages/MyOrder";
-import ProductDetail from "./pages/ProductDetail";
-import ChangePassword from "./pages/Auth/PasswordChange/ChangePassword";
-import ResetPassword from "./pages/Auth/PasswordChange/ResetPassword";
-import Profile from "./pages/Profile";
-import EditProfile from "./pages/EditProfile";
-import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
-import ProductLists from "./pages/ProductLists";
-import ChooseShipping from "./pages/ChooseShipping";
-import ForgotPassword from "./pages/Auth/ForgotPassword";
-import AddressList from "./pages/AddressList";
-import NewAddress from "./pages/NewAddress";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
+import SignIn from './pages/Auth/SignIn';
+import SignUp from './pages/Auth/SignUp';
+import CreatePassword from './pages/Auth/CreatePassword';
+import MyOrder from './pages/MyOrder';
+import ProductDetail from './pages/ProductDetail';
+import ChangePassword from './pages/Auth/PasswordChange/ChangePassword';
+import ResetPassword from './pages/Auth/PasswordChange/ResetPassword';
+import Profile from './pages/Profile';
+import EditProfile from './pages/EditProfile';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductLists from './pages/ProductLists';
+import ChooseShipping from './pages/ChooseShipping';
+import ForgotPassword from './pages/Auth/ForgotPassword';
+import AddressList from './pages/AddressList';
+import NewAddress from './pages/NewAddress';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
 
 class App extends React.Component {
   render() {
