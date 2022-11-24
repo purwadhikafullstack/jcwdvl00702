abc68c33f4204920ab50e3f1a971fbdae95d0598
unknown <glenchosua@gmail.com>
2022-11-24 13:42:25 +0700

    bug fix

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -1,44 +1,23 @@
 const express = require(`express`);
 const cors = require(`cors`);
-const mysql = require("mysql2");
-const db = require("./database");
+// const mysql = require("mysql2");
+const db = require("./models");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const customerRoute = require("./routes/customer");
 
+dotenv.config();
 const PORT = 3300;
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 (async () => {
   await db.sequelize.sync();
 })();
 
-// const db = mysql.createConnection({
-//   host: `localhost`,
-//   user: `root`,
-//   password: `jcwdvl07`,
-//   database: `db_ecom`,
-//   port: 3306,
-//   multipleStatements: true,
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     return console.error(`error : ${err.message}`);
-//   }
-//   console.log(`Connected to MySQL Server`);
-// });
-
-// app.get("/customer", (req, res) => {
-//   let scriptQuery = "Select * from customer";
-//   db.query(scriptQuery, (err, results) => {
-//     if (err) res.status(500).send(err);
-//     res.status(200).send(results);
-//   });
-// });
-
-app.get(`/`, (req, res) => {
-  res.status(200).send(`<h4>Integrated mysql with express</h4>`);
-});
+app.use("/api/customer", customerRoute);
 
 app.listen(PORT, () => console.log(`API running:`, PORT));
