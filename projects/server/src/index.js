373e817187c633912f7188d1337d8715a4598a03
deanfebrius <deanfebrius7@gmail.com>
2022-12-01 20:52:55 +0700

    MWA-15

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -5,6 +5,8 @@ const db = require("./models");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const customerRoute = require("./routes/customer");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 dotenv.config();
 const PORT = 3300;
@@ -13,6 +15,12 @@ const app = express();
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  "/profileimages",
+  express.static(path.join(__dirname, "../public/profileimages"))
+);
 
 (async () => {
   await db.sequelize.sync();
