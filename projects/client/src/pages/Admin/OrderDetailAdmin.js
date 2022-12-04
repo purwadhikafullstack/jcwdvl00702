78f2652e263d0125726a16c5abf72fc9e4bb2592
Cychlope <m.marcelinus@gmail.com>
2022-12-04 13:36:26 +0700

    Page Order Detail Admin

--- projects/client/src/pages/Admin/OrderDetailAdmin.js
+++ projects/client/src/pages/Admin/OrderDetailAdmin.js
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import { useState, Fragment } from 'react';
-import { ArrowBack } from '@mui/icons-material';
+import { Add, ArrowBack, Remove, ReceiptLong, SportsSoccerOutlined, MoreHoriz } from '@mui/icons-material';
+import { useHistory } from 'react-router-dom';
 import {
   Container,
   Button,
   IconButton,
+  InputBase,
+  Select,
+  MenuItem,
+  TextField,
+  Radio,
+  FormControl,
+  FormControlLabel,
+  RadioGroup,
+  Stack,
+  Pagination,
+  Menu,
   Stepper,
   Step,
   StepLabel,
@@ -12,17 +25,18 @@ import {
   Typography,
   Box,
 } from '@mui/material';
-import { useHistory } from 'react-router-dom';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+
+import { Link } from 'react-router-dom';
 
 import '../../assets/styles/OrderDetailAdmin.css';
 
 export default function OrderDetailAdmin() {
-  const history = useHistory();
   const goBack = () => {
-    history.goBack();
+    this.props.history.goBack();
   };
 
-  const [activeStep, setActiveStep] = useState(5);
+  const [activeStep, setActiveStep] = useState(1);
   const [paymentIsDone, setPaymentIsDone] = useState(false);
 
   const handleNext = () => {
@@ -58,46 +72,11 @@ export default function OrderDetailAdmin() {
       label: 'Received',
       description: `Order is received by User. Process is finished.`,
     },
-    // {
-    //   label: 'Cancelled',
-    //   description: `Order is cancelled`,
-    // },
+    {
+      label: 'Cancelled',
+      description: `Order is cancelled`,
+    },
   ];
-
-  const orderDetailCard = () => {
-    return (
-      <div className="od-card-main">
-        <div className="od-card-image">
-          <img
-            src="https://cf.shopee.co.id/file/442835254598ddfbf47d10830a99b3a8"
-            className="od-card-product"
-            alt="Product Image"
-          />
-        </div>
-        <div className="od-card-detail">
-          <div className="od-card-detail-name">Mercurial Vapor 8 Tipe ABC Besar</div>
-          <div className="od-card-detail-subname">White | shoes | 41</div>
-          <div className="od-card-detail-bottom">
-            <div className="od-card-detail-bottom-price">Rp. 2.250.000 ,-</div>
-          </div>
-          <Button
-            disabled
-            className="od-card-button-list"
-            sx={{
-              width: '50px',
-              borderRadius: '20px',
-              fontSize: '13px',
-              backgroundColor: 'gray',
-              border: 'none',
-              fontWeight: '700',
-              color: 'black',
-            }}>
-            1
-          </Button>
-        </div>
-      </div>
-    );
-  };
 
   const orderDetailConst = [
     { title: 'Created Date', value: '11-11-2022' },
@@ -115,11 +94,6 @@ export default function OrderDetailAdmin() {
       value:
         'Letjen S. Parman St No.kav.28 Central Park Mall Lantai 20 Banyak Toko Ke kanan dan ke kiri Letjen S. Parman St No.kav.28 Letjen S. Parman St No.kav.28 ',
     },
-  ];
-
-  const paymentDetail = [
-    { title: 'Total', value: 'Rp 126.789.999 ,-' },
-    { title: 'Payment Method', value: 'Bank Transfer' },
   ];
 
   return (
@@ -144,90 +118,65 @@ export default function OrderDetailAdmin() {
         </div>
 
         <div className="oda-step">
-          <div className="oda-step-text">Order Status</div>
+          <Stepper activeStep={activeStep} orientation="vertical">
+            {steps.map((step, index) => (
+              <Step key={step.label}>
+                <StepLabel
+                // optional={index === 2 ? <Typography variant="caption">Last step</Typography> : null}
+                >
+                  {step.label}
+                </StepLabel>
+                <StepContent>
+                  <Typography>{step.description}</Typography>
+                  <Box sx={{ mb: 2 }}>
+                    <div>
+                      {/* {index === steps.length - 1 || index === 0 ? (
+                        <Button disabled onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
+                          Waiting
+                        </Button>
+                      ) : (
+                        <Button variant="contained" onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
+                          Continue
+                        </Button>
+                      )} */}
 
-          <Box sx={{ mb: 2, fontFamily: 'Lora' }}>
-            <Stepper activeStep={activeStep} orientation="vertical">
-              {steps.map((step, index) => (
-                <Step key={step.label}>
-                  <StepLabel sx={{ fontFamily: 'Lora' }}>{step.label}</StepLabel>
-                  <StepContent>
-                    <Typography sx={{ fontFamily: 'Lora', fontSize: '11px' }}>{step.description}</Typography>
-                    <Box sx={{ mb: 2 }}>
-                      <div>
-                        {index === 0 || index === 3 ? (
-                          <Button disabled sx={{ mt: 1, mr: 1, fontFamily: 'Lora', fontSize: '12px' }}>
-                            Waiting...
+                      {/* <Button variant="contained" onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
+                        {index === steps.length - 1 || index === 0 ? 'Finish' : 'Continue'}
+                      </Button> */}
+                      {index === 0 ? (
+                        <Button disabled sx={{ mt: 1, mr: 1 }}>
+                          Waiting
+                        </Button>
+                      ) : null}
+
+                      {index === 1 ? (
+                        <>
+                          <Button variant="contained" onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
+                            Accept
                           </Button>
-                        ) : null}
-                        {index === 1 ? (
-                          <>
-                            <Button
-                              variant="contained"
-                              onClick={handleNext}
-                              sx={{ mt: 1, mr: 1, fontFamily: 'Lora', fontSize: '12px' }}>
-                              Accept
-                            </Button>
-                            <Button
-                              variant="contained"
-                              onClick={handleNext}
-                              sx={{ mt: 1, mr: 1, fontFamily: 'Lora', fontSize: '12px' }}>
-                              Reject
-                            </Button>
-                          </>
-                        ) : null}
-                        {index === 2 ? (
-                          <>
-                            <Button
-                              variant="contained"
-                              onClick={handleNext}
-                              sx={{ mt: 1, mr: 1, fontFamily: 'Lora', fontSize: '12px' }}>
-                              Done
-                            </Button>
-                          </>
-                        ) : null}
-                      </div>
-                    </Box>
-                  </StepContent>
-                </Step>
-              ))}
-            </Stepper>
-            {activeStep === steps.length && (
-              <Paper square elevation={0} sx={{ p: 3 }}>
-                <Typography sx={{ fontFamily: 'Lora', fontSize: '15px', fontWeight: 'bold' }}>
-                  Order is complete !
-                </Typography>
-              </Paper>
-            )}
-          </Box>
-        </div>
+                          <Button variant="contained" onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
+                            Reject
+                          </Button>
+                        </>
+                      ) : null}
 
-        <div className="oda-payment">
-          <div className="oda-payment-text">Proof of Payment</div>
-          <div className="oda-payment-detail">
-            {paymentDetail.map((item) => (
-              <div className="oda-payment-wrapper">
-                <div className="oda-payment-left">{item.title}</div>
-                <div className="oda-payment-right">{item.value}</div>
-              </div>
+                      {/* <Button disabled={index === 0} onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                        Back
+                      </Button> */}
+                    </div>
+                  </Box>
+                </StepContent>
+              </Step>
             ))}
-          </div>
-          <img
-            src="https://help.xendit.co/hc/article_attachments/4407785781389/Screen_Shot_2021-08-24_at_22.33.33.png"
-            alt="Payment Proof"
-            className="oda-payment-img"
-          />
-        </div>
-
-        <div className="oda-list">
-          <div className="oda-list-text">Items</div>
-          <div className="oda-list-card">
-            {orderDetailCard()}
-            {orderDetailCard()}
-            {orderDetailCard()}
-            {orderDetailCard()}
-            {orderDetailCard()}
-          </div>
+          </Stepper>
+          {activeStep === steps.length && (
+            <Paper square elevation={0} sx={{ p: 3 }}>
+              <Typography>All steps completed - you&apos;re finished</Typography>
+              <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+                Reset
+              </Button>
+            </Paper>
+          )}
         </div>
       </div>
     </Container>
